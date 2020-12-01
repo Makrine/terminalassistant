9385bdd4f3b75821fb85fef46ab6bdf9a60e362b
Makrine <66353680+Makrine@users.noreply.github.com>
2020-12-01 23:10:51 +0400

    Add files via upload
    ბევრი რამეა დასარედაქტირებელი

---
+++ Zodiac.cpp
@@ -0,0 +1,18 @@
+/****************************************\
+* Zodiac			        *
+* Written By Makrine Tskipurishvili     *
+* December 1, 2020		        *
+\****************************************/
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <fstream>
+#include "zodiac.h"
+using namespace std;
+
+int main() {
+	ZODIAC Z;
+	Z.ShowFirstPage();
+	return 0;
+}
+
