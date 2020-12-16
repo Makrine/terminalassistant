ccae2cbdc842ec115d9ff217c150735b73bc3163
Makrine <66353680+Makrine@users.noreply.github.com>
2020-12-16 22:22:12 +0400

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,254 @@
+#include <ncurses.h>
+#include "zodiac.h"
+#include <cstring>
+
+// source https://stackoverflow.com/questions/22399406/drawing-boxes-using-ncurses/22399551 FOR THE FRAME
+
+
+
+void menu(char* filename, class ZODIAC& A);
+void currentUser(class ZODIAC& A);
+void input(char* filename, class ZODIAC& A);
+
+
+void removeLine(char* filename, class ZODIAC& A) {
+
+	string target = A.GetCurrent();
+	string line;
+	ifstream myfile;
+	myfile.open(filename);
+	ofstream temp;
+	temp.open("temp.txt");
+
+	while(getline(myfile,line)) {
+		if(line == target) continue;
+		else temp << line << endl;
+	}
+
+	temp.close();
+	myfile.close();
+	remove(filename);
+	rename("temp.txt",filename);
+}
+
+
+void FRAME(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2-x1);
+    mvhline(y2, x1, 0, x2-x1);
+    mvvline(y1, x1, 0, y2-y1);
+    mvvline(y1, x2, 0, y2-y1);
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
+}
+
+void skeleton() {
+	FRAME(5, 20, 30, 120);
+	FRAME(4, 18, 31, 122);
+	mvprintw(2, 62, "WELCOME TO ZODIAC!");
+}
+
+
+void choice1(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	char sign[15];
+	strcpy(sign, A.GetSunSign(A.GetBirthdate()).c_str());
+	mvprintw(8, 22, "You are a %s! :)", sign);
+	
+	noecho();
+	curs_set(0);
+	mvprintw(10, 22, "Press any key to go back to menu...");
+	getch();
+	menu(filename, A);
+}
+void choice2(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	char sign[15];
+	strcpy(sign, A.GetChineseSign(A.GetBirthdate()).c_str());
+	mvprintw(8, 22, "You are a %s! :)", sign);
+	
+	noecho();
+	curs_set(0);
+	mvprintw(10, 22, "Press any key to go back to menu...");
+	getch();
+	menu(filename, A);
+}
+void choice3(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	int num;
+	num = A.GetLifePathNumber(A.GetBirthdate());
+	mvprintw(8, 22, "Your Life Path Number is %d! :)", num);
+	
+	noecho();
+	curs_set(0);
+	mvprintw(10, 22, "Press any key to go back to menu...");
+	getch();
+	menu(filename, A);
+}
+void choice4(char* filename, class ZODIAC& A) {
+
+}
+void choice5(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	
+	A.BlankFile(filename);
+
+	noecho();
+	curs_set(0);
+	mvprintw(8, 22, "User Data has been REMOVED");
+	mvprintw(10, 22, "Press Any Key to go back to menu...");
+	getch();
+	input(filename, A);
+}
+void choice6(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	
+	removeLine(filename, A);
+
+	noecho();
+	curs_set(0);
+	mvprintw(8, 22, "User: %s %s %s is REMOVED from database", A.GetName().c_str(), A.GetSurname().c_str(), A.GetBirthdate().c_str());
+	mvprintw(10, 22, "Press Any Key to go back to menu...");
+	getch();
+	input(filename, A);
+
+}
+void choice7(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	
+	input(filename, A);
+	menu(filename, A);
+
+}
+void choice8(char* filename, class ZODIAC& A) {
+
+}
+void choice(char* filename, class ZODIAC& A) {
+	echo();
+	curs_set(1);
+	move(0, 0);
+	int ch;
+	ch = getch();
+	getch();
+	
+	switch(ch) {
+		case '1':
+			choice1(filename, A);
+			break;
+		case '2':
+			choice2(filename, A);
+			break;
+		case '3':
+			choice3(filename, A);
+			break;
+		case '4':
+			choice4(filename, A);
+			break;
+		case '5':
+			choice5(filename, A);
+			break;
+		case '6':
+			choice6(filename, A);
+			break;
+		case '7':
+			choice7(filename, A);
+			break;
+		case '8':
+			choice8(filename, A);
+			break;
+	}
+
+}
+void menu(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	currentUser(A);
+	
+	mvprintw(7.5, 22, "Choose and type the corresponding digit :)");
+	mvprintw(8, 22, "1. Your Sun Sign");
+	mvprintw(9, 22, "2. Your Chinese Sign");
+	mvprintw(10, 22, "3. Your Life Path Number");
+	mvprintw(11, 22, "4. See User Data");
+	mvprintw(12, 22, "5. Delete User Data");
+	mvprintw(13, 22, "6. Delete Current Data");
+	mvprintw(14, 22, "7. Enter New Data");
+	mvprintw(15, 22, "8. Exit");
+	
+	choice(filename, A);
+}
+
+void input(char* filename, class ZODIAC& A) {
+	clear();
+	skeleton();
+	char name[20];
+	char surname[30];
+	char birthdate[12];
+	mvprintw(6, 22, "Please, enter your name, surname and a date of birth in order to continue. Your data will be saved");
+	mvprintw(7,22, "to the file you indicated above.");
+	echo();
+	curs_set(1);
+	mvprintw(13, 55, "NAME: ");
+	getstr(name);
+	
+	
+	mvprintw(14, 55, "SURNAME: ");
+	getstr(surname);
+
+	mvprintw(15, 55, "BIRTHDATE (dd/mm/yyyy): ");
+	mvprintw(16, 60, "(ex: 28/07/2000)");
+	move(15, 79);
+	getstr(birthdate);
+
+	A.SetName(name);
+	A.SetSurname(surname);
+	A.SetBirthdate(birthdate);
+	
+	A.PrintToFile(filename);
+	menu(filename, A);
+
+}
+
+void currentUser(class ZODIAC& A) {
+	mvprintw(6, 22, "NAME: %s\t\tSURNAME: %s\t\tBIRTHDATE: %s", A.GetName().c_str(), A.GetSurname().c_str(), A.GetBirthdate().c_str());
+}
+int main()
+{
+	ZODIAC A;
+	PEOPLE B;
+	initscr();			/* Start curses mode 		  */	
+	char name[20];
+	char surname[30];
+	char birthdate[12];		
+	char filename[50];
+	skeleton();
+	
+	mvprintw(6, 22, "Please enter a name for the database file: ");
+	getstr(filename);
+	char txt[5] = ".txt";
+	strcat(filename, txt);
+	
+	
+	A.BlankFile(filename);
+
+	input(filename, A);
+
+	menu(filename, A);
+
+	endwin();			/* End curses mode		  */
+
+	return 0;
+}
---
+++ zodiac.h
@@ -0,0 +1,206 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+
+bool cmp(const class ZODIAC&, const class ZODIAC&);
+
+
+class ZODIAC {
+private:
+	string name, surname, birthdate;
+	int dd, mm, yyyy;
+	
+public:
+	friend bool cmp(const class ZODIAC&, const class ZODIAC&);
+	friend class PEOPLE;
+	
+	string GetCurrent() {
+		string current = "";
+		current += " " + name;
+		for(int i=name.length()+2; i<=21; i++) {
+			current += " ";
+		}
+		current += surname;
+		for(int i=surname.length()+22; i<=48; i++) {
+			current += " ";
+		}
+		current += birthdate;
+		
+		return current;
+	}
+	void BlankFile(string filename) {
+		ofstream myfile;
+		myfile.open(filename);
+		myfile << " Name" << "                " << "Surname" << "                    " << "Birthdate" << endl;
+		myfile << "_________________________________________________________________" << endl;
+	}
+	void PrintToFile(string filename) {
+		ofstream myfile;
+		myfile.open(filename, ios_base::app);
+		myfile << " " << name;
+		for(int i=name.length()+2; i<=21; i++) {
+			myfile << " ";
+		}
+		myfile << surname;
+		for(int i=surname.length()+22; i<=48; i++) {
+			myfile << " ";
+		}
+		myfile << birthdate << endl;
+		myfile.close();
+	}
+	void SetName(string nm) {name = nm;}
+	void SetSurname(string snm) {surname = snm;}
+	void SetBirthdate(string bd) {birthdate = bd;}
+	string GetName() {return name;}
+	string GetSurname() {return surname;}
+	string GetBirthdate() {return birthdate;}
+	void Birthdate(string bd) {
+		string s_dd = bd.substr(0,2);
+		string s_mm = bd.substr(3,2);
+		string s_yyyy = bd.substr(6,4);
+		int a, b, c, d;
+		a = s_dd[0] - '0';
+		b = s_dd[1] - '0';
+		dd = a*10 + b;
+
+		a = s_mm[0] - '0';
+		b = s_mm[1] - '0';
+		mm = a*10 + b;
+
+		a = s_yyyy[0] - '0';
+		b = s_yyyy[1] - '0';
+		c = s_yyyy[2] - '0';
+		d = s_yyyy[3] - '0';
+		yyyy = a*1000 + b*100 + c*10 + d;
+
+	}
+	string GetSunSign(string bd) {
+		Birthdate(bd);
+		if((dd >= 21 && dd <= 31 && mm == 3) || (dd >= 1 && dd <= 19 && mm == 4)) return "Aries";
+		else if((dd >= 20 && dd <= 30 && mm == 4) || (dd >= 1 && dd <= 20 && mm == 5)) return "Taurus";
+		else if((dd >= 21 && dd <= 31 && mm == 5) || (dd >= 1 && dd <= 20 && mm == 6)) return "Gemini";
+		else if((dd >= 21 && dd <= 30 && mm == 6) || (dd >= 1 && dd <= 22 && mm == 7)) return "Cancer";
+		else if((dd >= 23 && dd <= 31 && mm == 7) || (dd >= 1 && dd <= 22 && mm == 8)) return "Leo";
+		else if((dd >= 23 && dd <= 30 && mm == 8) || (dd >= 1 && dd <= 22 && mm == 9)) return "Virgo";
+		else if((dd >= 23 && dd <= 31 && mm == 9) || (dd >= 1 && dd <= 22 && mm == 10)) return "Libra";
+		else if((dd >= 23 && dd <= 30 && mm == 10) || (dd >= 1 && dd <= 21 && mm == 11)) return "Scorpio";
+		else if((dd >= 22 && dd <= 31 && mm == 11) || (dd >= 1 && dd <= 21 && mm == 12)) return "Sagittarius";
+		else if((dd >= 22 && dd <= 30 && mm == 12) || (dd >= 1 && dd <= 19 && mm == 1)) return "Capricorn";
+		else if((dd >= 20 && dd <= 31 && mm == 1) || (dd >= 1 && dd <= 18 && mm == 2)) return "Aquarius";
+		return "ERROR";
+	}
+	string GetChineseSign(string bd) {
+		Birthdate(bd);
+		string ChSign;
+		switch((yyyy-1912)%12) {
+			case 0:
+				ChSign = "Rat";
+				break;
+			case 1:
+				ChSign = "Ox";
+				break;
+			case 2:
+				ChSign = "Tiger";
+				break;
+			case 3:
+				ChSign = "Rabbit";
+				break;
+			case 4:
+				ChSign = "Dragon";
+				break;
+			case 5:
+				ChSign = "Snake";
+				break;
+			case 6:
+				ChSign = "Horse";
+				break;
+			case 7:
+				ChSign = "Goat";
+				break;
+			case 8:
+				ChSign = "Monkey";
+				break;
+			case 9:
+				ChSign = "Rooster";
+				break;
+			case 10:
+				ChSign = "Dog";
+				break;
+			case 11:
+				ChSign = "Pig";
+				break;
+		}
+		return ChSign;
+	}
+	int GetLifePathNumber(string bd) {
+		Birthdate(bd);
+		int num, a, b, c, e;
+		num = dd+mm+yyyy;
+		while(num/10 >= 1) {
+			a = num/1000;
+			num = num - a*1000;
+			b = num/100;
+			num = num - b*100;
+			c = num/10;
+			num = num - c*10;
+			e = num;
+			num = a + b + c + e;
+		}
+		return num;
+	}
+
+
+
+};
+
+bool cmp(const class ZODIAC& p, const class ZODIAC& q) {
+	if(p.name == q.name) return(p.surname < q.surname);
+	else return (p.name < q.name);
+}
+
+class PEOPLE {
+private:
+	class ZODIAC* list;
+	int size = -2; //size of ZODIAC* list
+public:
+
+	void SetSize(string filename) {int n = GetNumOfUsers(filename); list = new class ZODIAC[n];}
+	void FileToList(string filename) {
+		ifstream file(filename);
+		int c = 0, i = 0, j = 0;
+		string words[3], line;
+		for(;getline(file, line);) {
+			c++;
+			if(c<3) continue;
+			else {
+				j = 0;
+				stringstream iss(line);
+				while(iss >> words[j++]);
+				list[i].SetName(words[0]);
+				list[i].SetSurname(words[1]);
+				list[i].SetBirthdate(words[2]);
+			}
+			i++;
+		}
+	}
+	int GetNumOfUsers(string filename) {
+		string line;
+		ifstream file(filename);
+		while(getline(file, line)) size++;
+		return size;
+	}
+	void Sort() {sort(list, list+size, cmp);}
+	void PrintToFile(string filename) {list->BlankFile(filename); for(int i=0; i <size; i++) list[i].PrintToFile(filename);}
+	//void Print() {for(int i=0; i <size; i++) cout<<list[i].GetName() << " " << list[i].GetSurname() << " " <<list[i].GetBirthdate() << endl;}
+/*	
+	void PrintFile(string filename) {
+		string line;
+		ifstream file(filename);
+		while(getline(file, line)) cout << line << endl;
+	}
+*/
+};
+
