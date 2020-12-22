c1bf6afd5fe7e2d6849cc4f1e0a4d5d2c477abcb
Makrine <66353680+Makrine@users.noreply.github.com>
2020-12-22 23:13:43 +0400

    Add files via upload

---
+++ desc.h
@@ -0,0 +1,170 @@
+#include <ncurses.h>
+using namespace std;
+void Aries() {
+	mvprintw(16, 23, "The Aries zodiac sign is the 1st sign of the zodiac. The Aries people are willful, feisty,");
+	mvprintw(17, 23, "self-centered, courageous, bold, foolhardy, independent and straightforward.");
+}
+void Taurus() {
+	mvprintw(16, 23, "The Taurus zodiac sign is the 2nd sign of the zodiac. The Taurus people are practical, creative,");
+	mvprintw(17, 23, "loyal, possessive, temperamental, sensually indulgent, down-to-earth, dependable,");
+	mvprintw(18, 23, "persistent and practical.");
+}
+
+void Gemini() {
+	mvprintw(16, 23, "The Gemini zodiac sign is the 3rd sign of the zodiac. The Gemini people are curious, elusive, ");
+	mvprintw(17, 23, "unpredictable, changeable, versatile, childlike, romantic, playful, friendly,");
+	mvprintw(18, 23, "talkative and have a keen intellect.");
+}
+void Cancer() {
+	mvprintw(16, 23, "The Cancer zodiac sign is the 4th sign of the zodiac. The Cancer people are cautious,");
+	mvprintw(17, 23, "protective, nurturing, secretive, instinctive, needy, sensitive, funny,");
+	mvprintw(18, 23, "empathetic and deeply complex people.");
+}
+
+void Leo() {
+	mvprintw(16, 23, "The Leo zodiac sign is the 5th sign of the zodiac. The Leos are distinctive, provocative,");
+	mvprintw(17, 23, "demanding, goal-oriented, flamboyant, self-made, warm, outgoing, sincere and loyal people.");
+}
+void Virgo() {
+	mvprintw(16, 23, "The Virgo zodiac sign is the 6th sign of the zodiac. The Virgos are discriminating,");
+	mvprintw(17, 23, "obsessive, realistic, analytical, reliable, self-contained,");
+	mvprintw(17, 23, "knowledgeable, predictable, obsessive, discriminating, street-smart, detailed and modest people.");
+}
+void Libra() {
+	mvprintw(16, 23, "The Libra zodiac sign is the 7th sign of the zodiac. Librans are harmonious,");
+	mvprintw(17, 23, "civilized, intellectual, sophisticated,seductive, elegant, creative,");
+	mvprintw(18, 23, "witty, balanced, sociable and people who maintain their status quo.");
+}
+void Scorpio() {
+	mvprintw(16, 23, "The Scorpio zodiac sign is the 8th sign of the zodiac. The Scorpio people are");
+	mvprintw(17, 23, "intense, magnetic, erotic, challenging, secretive, powerful, broody,");
+	mvprintw(18, 23, "passionate, immovable and penetrating.");
+}
+
+void Sagittarius() {
+	mvprintw(16, 23, "The Sagittarius zodiac sign is the 9th sign of the zodiac.");
+	mvprintw(17, 23, "The Sagittarius are adventurous, hilarious, extrovert, romantics,");
+	mvprintw(18, 23, "spirited, unstoppable, generous, happy and open-minded.");
+}
+void Capricorn() {
+	mvprintw(16, 23, "The Capricorn zodiac sign is the 10th sign of the zodiac. The Capricorn people are organized, ");
+	mvprintw(17, 23, "respectful, devoted, classy, materialistic, serious, staid, ambitious and practical.");
+}
+void Aquarius() {
+	mvprintw(16, 23, "The Aquarius zodiac sign is the 11th sign of the zodiac. Aquarius people are original,");
+	mvprintw(17, 23, "idealistic, rebellious, independent, inventors, open minded and honest.");
+}
+void Pisces() {
+	mvprintw(16, 23, "The Pisces zodiac sign is the 12th sign of the zodiac. Pisceans are dreamy, erratic, creative,");
+	mvprintw(17, 23, "romantic, compassionate, elusive, imaginative, sensitive and kind people.");
+}
+
+
+
+void Rat() {
+	mvprintw(16, 23, "People born in the Year of the Rat are quite charismatic.");
+}
+void Ox() {
+	mvprintw(16, 23, "People born in the Year of the Ox are direct and know what they want.");
+}
+
+void Tiger() {
+	mvprintw(16, 23, "Anyone born under the Year of the Tiger is thought to be courageous.");
+}
+void Rabbit() {
+	mvprintw(16, 23, "People born in the Year of the Rabbit are genuine and like everyone to get along.");
+}
+
+void Dragon() {
+	mvprintw(16, 23, "People born under the Year of the Dragon are spirited and considered lucky.");
+}
+void Snake() {
+	mvprintw(16, 23, "People born in the Year of the Snake are creative.");
+}
+void Horse() {
+	mvprintw(16, 23, "Anyone born in the Year of the Horse is considered pretty laid-back and benevolent.");
+}
+void Goat() {
+	mvprintw(16, 23, "People born in the Year of the Goat are considered artsy.");
+}
+
+void Monkey() {
+	mvprintw(16, 23, "Anyone born in the Year of the Monkey is thought to be humorous and optimistic.");
+}
+void Rooster() {
+	mvprintw(16, 23, "People born in the Year of the Rooster are very ethical.");
+}
+void Dog() {
+	mvprintw(16, 23, "Just like the animal the year is based on, anyone born in the Year of the Dog");
+	mvprintw(17, 23, "is thought to be extremely loyal.");
+}
+void Pig() {
+	mvprintw(16, 23, "People born in the Year of the Pig are forgiving and genial.");
+}
+
+
+void One() {
+	mvprintw(16, 23, "1 are natural born leaders. With the number one beginning our counting system, ");
+	mvprintw(17, 23, "this makes 1 Life Paths naturally first in many areas of life. They are ");
+	mvprintw(18, 23, "usually competitive and have a knack at being first. This Life Path is all about ");
+	mvprintw(19, 23, "independence in mind and actions. They are not afraid of putting themselves ");
+	mvprintw(20,23, "out there where others are too cautious. However, there can be a ");
+	mvprintw(21,23, "tendency with ones to be self indulgent and selfish.");
+}
+
+void Two() {
+	mvprintw(16, 23, "2 are your peacemakers. Number 2 people are naturally good with people ");
+	mvprintw(17, 23, "and seek to harmonize and balance people, places, and things. They have no");
+	mvprintw(18, 23, "problem taking the lead in any situation but, are usually more comfortable in a ");
+	mvprintw(19, 23, "supporting role. 2 Life Paths are business-minded and love projects that involve ");
+	mvprintw(20, 23, "others. This is also a very intuitive number and many 2 are naturally psychic.");
+}
+void Three() {
+	mvprintw(16, 23, "3s are your communicators and motivators. 3 is the number of the child so this ");
+	mvprintw(17, 23, "Life Path has a youthful approach to life. They have a funny sense of humor and ");
+	mvprintw(18, 23, "tend to not take life too seriously. These Life Paths have a way with words! ");
+	mvprintw(19, 23, "Writing, communicating, and self-expression come easy to 3. Three people");
+	mvprintw(20, 23, "are also highly creative and are attracted to most artistic expressions.");
+}
+void Four() {
+	mvprintw(16, 23, "4 are your students and teachers. 4 are very inquisitive and love learning ");
+	mvprintw(17, 23, "new things. They pay attention to the details! 4 are very comfortable at home");
+	mvprintw(18, 23, "and are also business-minded. It is not uncommon to find 4s running a home ");
+	mvprintw(19, 23, "based business. They love structure and foundation and are usually not spontanous");
+}
+void Five() {
+	mvprintw(16, 23, "5s are your adventurers. 5 people are all about action and getting things done, ");
+	mvprintw(17, 23, "yesterday. This is the number of the traveler. 5s love to be on the go and are ");
+	mvprintw(18, 23, "not comfortable staying in one place for too long and this includes mentally. 5 ");
+	mvprintw(19, 23, "is a very mental number and these Life Paths have the ability to materialize and");
+	mvprintw(20, 23, "actualize what they conceive in their minds. They are very much into leading ");
+	mvprintw(21, 23, "and leadership roles. Boredom is 5s Kryptonite so, they like to stay very busy.");
+}
+void Six() {
+	mvprintw(16, 23, "6s are your nurturers and parent figures. 6s are usually very balanced");
+	mvprintw(17, 23, "physically and mentally. They have an innate ability to counsel those around");
+	mvprintw(18, 23, "them in many areas of life. 6s are extremely creative and are never at a loss for ");
+	mvprintw(19, 23, "artistic and creative endeavors. They seek to express themselves in a loving ");
+	mvprintw(20, 23, "and caring way. On the flip side, these Life Paths can be overbearing with their ");
+	mvprintw(21, 23, "parenting energy and treat others as if they were their children!");
+}
+void Seven() {
+	mvprintw(16, 23, "7s are your spiritual seekers and investigators. Need to find out about any ");
+	mvprintw(17, 23, "subject? 7s are the ones to call! With 7 being a highly mental number, they ");
+	mvprintw(18, 23, "have no problem with seeking out the depth of any subject. Many 7s are ");
+	mvprintw(19, 23, "comfortable being by themselves and have the tendency to be aloof or loners. ");
+
+}
+void Eight() {
+	mvprintw(16, 23, "8s are your business leaders and executives. 8s are at their best when they are ");
+	mvprintw(17, 23, "balanced spiritually and physically. They are into manifestation and seek to ");
+	mvprintw(18, 23, "have physical things along with spiritual enlightenment. 8s energy is vital and ");
+	mvprintw(19, 23, "they rarely go unnoticed. ");
+}
+void Nine() {
+	mvprintw(16, 23, "9s are your humanitarians and versatile souls. 9s at the highest vibration seek");
+	mvprintw(17, 23, "to make the world a better place. These are the people who will assist anyone ");
+	mvprintw(18, 23, "with anything as long as it is for the greater good. With an abundance of life ");
+	mvprintw(19, 23, "experience, they are comfortable helping people with challenges. ");
+}
+
