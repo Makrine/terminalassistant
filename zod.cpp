caeea1cad4925003c3b495cf17bc55eddd63023e
Makrine <66353680+Makrine@users.noreply.github.com>
2020-12-22 21:54:12 +0400

    Update zod.cpp

--- zod.cpp
+++ zod.cpp
@@ -4,25 +4,18 @@
 
 // source https://stackoverflow.com/questions/22399406/drawing-boxes-using-ncurses/22399551 FOR THE FRAME
 
+	ZODIAC A;
+	PEOPLE B;
 
-void removeLine(char* filename, class ZODIAC& A);
-void FRAME(int y1, int x1, int y2, int x2);
-void skeleton();
-void choice1(char* filename, class ZODIAC& A);
-void choice2(char* filename, class ZODIAC& A);
-void choice3(char* filename, class ZODIAC& A);
-void choice4(char* filename, class ZODIAC& A);
-void choice5(char* filename, class ZODIAC& A);
-void choice6(char* filename, class ZODIAC& A);
-void choice7(char* filename, class ZODIAC& A);
-void choice8(char* filename, class ZODIAC& A);
-void choice(char* filename, class ZODIAC& A);
-void menu(char* filename, class ZODIAC& A);
+void menu(char* filename);
 void currentUser(class ZODIAC& A);
-void input(char* filename, class ZODIAC& A);
-
-
-void removeLine(char* filename, class ZODIAC& A) {
+void input(char* filename);
+void PrintFile(char* filename) {
+	string line;
+	ifstream file(filename);
+	for(int i=0;getline(file, line);i++) mvprintw(i, 0, "%s", line.c_str());
+}
+void removeLine(char* filename) {
 
 	string target = A.GetCurrent();
 	string line;
@@ -62,7 +55,7 @@ void skeleton() {
 }
 
 
-void choice1(char* filename, class ZODIAC& A) {
+void choice1(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
@@ -74,9 +67,9 @@ void choice1(char* filename, class ZODIAC& A) {
 	curs_set(0);
 	mvprintw(10, 22, "Press any key to go back to menu...");
 	getch();
-	menu(filename, A);
+	menu(filename);
 }
-void choice2(char* filename, class ZODIAC& A) {
+void choice2(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
@@ -88,9 +81,9 @@ void choice2(char* filename, class ZODIAC& A) {
 	curs_set(0);
 	mvprintw(10, 22, "Press any key to go back to menu...");
 	getch();
-	menu(filename, A);
+	menu(filename);
 }
-void choice3(char* filename, class ZODIAC& A) {
+void choice3(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
@@ -102,12 +95,18 @@ void choice3(char* filename, class ZODIAC& A) {
 	curs_set(0);
 	mvprintw(10, 22, "Press any key to go back to menu...");
 	getch();
-	menu(filename, A);
+	menu(filename);
 }
-void choice4(char* filename, class ZODIAC& A) {
-//TODO
+void choice4(char* filename) {
+	clear();
+	noecho();
+	curs_set(0);
+	PrintFile(filename);
+	printw("\n\nPress any key to go to menu...");
+	getch();
+	menu(filename);
 }
-void choice5(char* filename, class ZODIAC& A) {
+void choice5(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
@@ -119,76 +118,80 @@ void choice5(char* filename, class ZODIAC& A) {
 	mvprintw(8, 22, "User Data has been REMOVED");
 	mvprintw(10, 22, "Press Any Key to go back to menu...");
 	getch();
-	input(filename, A);
+	input(filename);
 }
-void choice6(char* filename, class ZODIAC& A) {
+void choice6(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
 	
-	removeLine(filename, A);
+	removeLine(filename);
 
 	noecho();
 	curs_set(0);
 	mvprintw(8, 22, "User: %s %s %s is REMOVED from database", A.GetName().c_str(), A.GetSurname().c_str(), A.GetBirthdate().c_str());
 	mvprintw(10, 22, "Press Any Key to go back to menu...");
 	getch();
-	input(filename, A);
+	input(filename);
 
 }
-void choice7(char* filename, class ZODIAC& A) {
+void choice7(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
 	
-	input(filename, A);
-	menu(filename, A);
+	input(filename);
+	menu(filename);
 
 }
-void choice8(char* filename, class ZODIAC& A) {
-//TODO
+
+void choice8() {
+/*	system("clear");
+	exit(0);
+*/
+	endwin();
+	refresh();
 }
-void choice(char* filename, class ZODIAC& A) {
+void choice(char* filename) {
 	echo();
 	curs_set(1);
-	move(0, 0);
+	move(35, 1);
 	int ch;
 	ch = getch();
 	getch();
 	
 	switch(ch) {
 		case '1':
-			choice1(filename, A);
+			choice1(filename);
 			break;
 		case '2':
-			choice2(filename, A);
+			choice2(filename);
 			break;
 		case '3':
-			choice3(filename, A);
+			choice3(filename);
 			break;
 		case '4':
-			choice4(filename, A);
+			choice4(filename);
 			break;
 		case '5':
-			choice5(filename, A);
+			choice5(filename);
 			break;
 		case '6':
-			choice6(filename, A);
+			choice6(filename);
 			break;
 		case '7':
-			choice7(filename, A);
+			choice7(filename);
 			break;
 		case '8':
-			choice8(filename, A);
+			choice8();
 			break;
 	}
 
 }
-void menu(char* filename, class ZODIAC& A) {
+void menu(char* filename) {
 	clear();
 	skeleton();
 	currentUser(A);
-	
 	mvprintw(7.5, 22, "Choose and type the corresponding digit :)");
 	mvprintw(8, 22, "1. Your Sun Sign");
 	mvprintw(9, 22, "2. Your Chinese Sign");
@@ -198,11 +201,20 @@ void menu(char* filename, class ZODIAC& A) {
 	mvprintw(13, 22, "6. Delete Current Data");
 	mvprintw(14, 22, "7. Enter New Data");
 	mvprintw(15, 22, "8. Exit");
-	
-	choice(filename, A);
+	choice(filename);
 }
-
-void input(char* filename, class ZODIAC& A) {
+void Check(char* filename, char* name, char* surname, char* birthdate) {
+	bool flag = true;
+	if(A.CheckNameSurname(name)) A.SetName(name);
+	else {mvprintw(13, 16, "Invalid Name!"); flag=false;}
+	if(A.CheckNameSurname(surname)) A.SetSurname(surname);
+	else {mvprintw(14, 16, "Invalid Surname!"); flag=false;}
+	if(A.CheckDate(birthdate)) A.SetBirthdate(birthdate);
+	else {mvprintw(15, 16, "Invalid Birthdate!"); flag=false;}
+	
+	if(!flag) {mvprintw(20, 55, "Enter any key to input data again..."); getch(); input(filename);}
+}
+void input(char* filename) {
 	clear();
 	skeleton();
 	char name[20];
@@ -214,50 +226,47 @@ void input(char* filename, class ZODIAC& A) {
 	curs_set(1);
 	mvprintw(13, 55, "NAME: ");
 	getstr(name);
-	
-	
 	mvprintw(14, 55, "SURNAME: ");
 	getstr(surname);
-
 	mvprintw(15, 55, "BIRTHDATE (dd/mm/yyyy): ");
 	mvprintw(16, 60, "(ex: 28/07/2000)");
 	move(15, 79);
 	getstr(birthdate);
-
-	A.SetName(name);
-	A.SetSurname(surname);
-	A.SetBirthdate(birthdate);
-	
+	Check(filename, name, surname, birthdate);
 	A.PrintToFile(filename);
-	menu(filename, A);
+
+	//sorting starts
+	B.SetSize(filename);
+	B.FileToList(filename);
+	B.Sort();
+	B.PrintToFile(filename);
+	//sorting ends
+	menu(filename);
 
 }
 
 void currentUser(class ZODIAC& A) {
 	mvprintw(6, 22, "NAME: %s\t\tSURNAME: %s\t\tBIRTHDATE: %s", A.GetName().c_str(), A.GetSurname().c_str(), A.GetBirthdate().c_str());
 }
+
 int main()
 {
-	ZODIAC A;
-	PEOPLE B;
+
 	initscr();			/* Start curses mode 		  */	
 	char name[20];
 	char surname[30];
-	char birthdate[12];		
+	char birthdate[12];
 	char filename[50];
 	skeleton();
-	
 	mvprintw(6, 22, "Please enter a name for the database file: ");
 	getstr(filename);
 	char txt[5] = ".txt";
 	strcat(filename, txt);
-	
-	
 	A.BlankFile(filename);
 
-	input(filename, A);
+	input(filename);
 
-	menu(filename, A);
+	menu(filename);
 
 	endwin();			/* End curses mode		  */
 
