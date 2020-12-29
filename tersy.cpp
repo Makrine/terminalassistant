5f9615ce7bd16f8e7038d25ebefc7128fd55663e
Makrine <66353680+Makrine@users.noreply.github.com>
2020-12-29 15:07:04 +0400

    Update tersy.cpp

--- tersy.cpp
+++ tersy.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char** argv) {
 	if(argc < 2) {cout << "\nThis is TERSY!\nFor help please type \n\n./tersy -help\n\n in your terminal :)\n" << endl; return 0;}
 	else {
-		if(!strcmp(argv[1], "-help")) cout << "\nTERSY has a few features to have fun with.\n1)type\n./tersy -zodiac\n and TERY will be your personal Guru.\n2)Type \n./tersy -shoplist\nAnd TERSY will help you in your shopping!\n :)" << endl;
+		if(!strcmp(argv[1], "-help")) cout << "\nTERSY has a few features to have fun with.\n1)type\n./tersy -zodiac\n and TERSY will be your personal Guru.\n2)Type \n./tersy -shoplist\nAnd TERSY will help you in your shopping!\n :)" << endl;
 		else if(!strcmp(argv[1], "-zodiac")) {cout << "Opening zodiac..."<< endl; sleep(1); string f = "./zod"; system(f.c_str());}
 		else if(!strcmp(argv[1], "-shoplist")) {cout << "Opening shoplist..."<< endl;sleep(1); string f = "./SHOPLIST"; system(f.c_str());}
 		else cout << "Please type -help for help!" << endl;
