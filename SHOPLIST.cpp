7692e9eba82f8bc4837c5bba425040a6c12787d1
mixa <67707862+MmixaA@users.noreply.github.com>
2020-12-30 11:10:08 +0400

    Update SHOPLIST.cpp

--- SHOPLIST.cpp
+++ SHOPLIST.cpp
@@ -2,14 +2,15 @@
 #include<iomanip>
 #include<algorithm>
 #include <ncurses.h>
+#include <ctype.h>
 using namespace std;
 int i,size=1000,l; //q moved to string
-string product,s, q;
-char c,n='0';
+string p,t,q,s;
+char c,n='0',productChar[20],typeChar[20],quantityChar[20];
 class shoplist
 {
 	private:
-		string name,type,quantity;
+		string name,quantity,type;
 	public:
 		// to show information
 		void printdata(int i)
@@ -29,20 +30,11 @@ class shoplist
 			quantity=to_string(addstr);
 		}
 		//read information
-		void Read(string x)
+		void Read(string x,string y,string z)
 		{
-			char quantityChar[20]; //create char array for quantity
-			char typeChar[20];//create char array for type
 			name=x;
-			printw("Enter the Quantity: ");
-			getstr(quantityChar);
-			printw("Enter the type: ");
-			getstr(typeChar);
-			//convert char array to string and assign them to string quantity and string type
-			string str1(quantityChar);
-			string str2(typeChar);
-			quantity = quantityChar;
-			type = typeChar;
+            quantity=y;
+			type=z;
 		}
 		string checkn()
 		{
@@ -85,32 +77,42 @@ void convert(string &x)
     	x[a]+=32;
 	}
 }
-void DeleteElement(int &sz, shoplist* arr, string n, string t) //function to delete an element. 
-{	
-	if(t == "0") 						//if type is 0 then it will take care of the name
+void DeleteElement(int &sz, shoplist* arr, string n, string t) //function to delete an element.
+{
+	if(t=="0") 						//if type is 0 then it will take care of the name
 	{
-		int i;	
-		for (i = 0; i < sz; i++) 
-			if (arr[i].name == n) break;   
-		if (i < sz) 
+		int i;
+		for(i=0;i<sz;i++)
+		if(arr[i].name==n)
+		break;
+		if(i<sz)
 		{
-			sz = sz - 1; 
-			for (int j = i; j < sz; j++) 
-			arr[j] = arr[j+1]; 
-		} 
+			sz--;
+			for(int j=i;j<sz;j++)
+			arr[j]=arr[j+1];
+		}
 	}
-	if(n == "0")					//if name is 0 then it will take care of the name
+	if(n=="0")					//if name is 0 then it will take care of the name
 	{
-		int i; 
-		for (i = 0; i < sz; i++) 
-			if (arr[i].type == t) break;   
-		if (i < sz)
+		int i;
+		for(i=0;i<sz;i++)
+		if(arr[i].type==t)
+		break;
+		if(i<sz)
 		{
-			sz = sz - 1; 
-			for (int j = i; j < sz; j++) 
-			arr[j] = arr[j+1]; 
-		} 
+			sz--;
+			for(int j=i;j<sz;j++)
+			arr[j]=arr[j+1];
+		}
 	}
+}
+bool check(string x)
+{
+	int len=x.length();
+	for(int i=0;i<len;i++)
+	if(!isdigit(x[i]))
+	return false;
+	return true;
 }
 int main()
 {
@@ -129,29 +131,55 @@ int main()
 		if(n=='1')
 		{
 			printw("\nEnter the product name: ");
-			char productChar[20]; //create char array for product
 			getstr(productChar);
 			string str3(productChar); //convert char array to string
-			product = productChar; //assign to product
-			convert(product);
+			p = productChar; //assign to product
+			convert(p);
 			for(i=0;i<k;i++)
-			if(product==a[i].checkn())
+			if(p==a[i].checkn())
 			break;
-			if(product!=a[i].checkn())
+			if(p!=a[i].checkn())
 			{
-				a[k].Read(product);
-				k++;
-				printw("\n\nItem added to the list!\n\nPress any key to continue..."); getch(); //Item added to the list!
+				//create char array for quantity
+				//create char array for type
+				printw("Enter the Quantity: ");
+				getstr(quantityChar);
+				string str1(quantityChar);
+				q=quantityChar;
+				if(check(q))
+				{
+					printw("Enter the type: ");
+					getstr(typeChar);
+					//convert char array to string and assign them to string quantity and string type
+					string str2(typeChar);
+					t=typeChar;
+					convert(t);
+					a[k].Read(p,q,t);
+					k++;
+					printw("\n\nItem added to the list!\n\nPress any key to continue...");
+					getch();
+				}
+				else
+				{
+					printw("\n\nyou must to add number to the Quantity!\n\nPress any key to continue..."); getch();
+				}//Item added to the list!
 			}
 			else
 			{
 				printw("\nEnter the Quantity: ");
-				char qChar[20]; //create char array for q
-				getstr(qChar);
-				string str4(qChar);//convert char array to string
-				q = qChar;//assign to q
-				a[i].add(q);
-				printw("\n\nItem added to the list!\n\nPress any key to continue..."); getch(); //Item added to the list!
+				getstr(quantityChar);
+				string str4(quantityChar);//convert char array to string
+				q=quantityChar;
+				if(check(q))
+				{
+					a[i].add(q);
+					printw("\n\nItem added to the list!\n\nPress any key to continue...");
+                    getch(); //Item added to the list!
+                }
+                else
+                {
+					printw("\n\nyou must to add number to the Quantity!\n\nPress any key to continue..."); getch();
+				}
 			}
 		}
 		// for showing date
@@ -169,7 +197,6 @@ int main()
 			}
    			printw("\n\nPress any key to continue...");
    			getch();}
-
 		}
 		// for sorting and showing data
 		else if(n=='3')
@@ -207,33 +234,23 @@ int main()
 		//for removing elemnt
 		else if(n=='4')
 		{
-			if(k==0) {printw("\n\nNo items in the list..."); printw("\n\nPress any key to continue..."); getch();} //in case of no items
-			else {
+			if(k==0)
+			{
+				printw("\n\nNo items in the list...");
+				printw("\n\nPress any key to continue...");
+				getch();
+			} //in case of no items
 			printw("\nIn order to remove element by name click 1");
 			printw("\nIn order to remove elements by type click 2\n\n");
    			c = getch();  //getch(c) - wrong, getch() takes no arguments
 			if(c=='1')
 			{
 				printw("\nEnter the product name: ");
-    				char sChar[20]; //create char array for s
+    			char sChar[20]; //create char array for s
 				getstr(sChar);
 				string str5(sChar); //convert char array to string
 				s = sChar; //assign to s
-				
 				DeleteElement(k, a, s, "0");
-				
-				/*
-				for(i=0;i<k;i++)
-				if(s==a[i].checkn())
-				break;
-				k--;
-				for(;i<k;i++)
-				{
-					a[i].checkn()==a[i+1].checkn();
-					a[i].checkt()==a[i+1].checkt();
-					a[i].checkq()==a[i+1].checkq();
-				}
-				*/
 			}
 			else if(c=='2')
 			{
@@ -242,25 +259,14 @@ int main()
 				getstr(sChar);
 				string str6(sChar); //convert char array to string
 				s = sChar; //assign to s
-				
 				DeleteElement(k, a, "0", s);
-				/*
-				for(i=0;i<k;i++)
-				if(s==a[i].checkt())
-				{
-	 				a[i].checkn()==a[i+1].checkn();
-					a[i].checkt()==a[i+1].checkt();
-					a[i].checkq()==a[i+1].checkq();
-					i--;
-					k--;
-				}*/
+
 			}
 			else
 			{
 				printw("\nIncorrect charecter try again\n\n");
 	   			printw("\n\nPress any key to continue...");
    				getch();
-			}
 			}
 		}
 		//for finding elemet
@@ -273,7 +279,8 @@ int main()
 			string str7(sChar); //convert char array to string
 			s = sChar; //assign to s
 			clear();
-			for(i=0;i<k;i++) {
+			for(i=0;i<k;i++)
+			{
 				if(s==a[i].checkn())
 				break;
 			}
@@ -288,9 +295,8 @@ int main()
    				printw("\n\nPress any key to continue...");
    				getch();
    			}
-
 		}
-		else if(n == '6') 
+		else if(n == '6')
 		{
 			delwin(stdscr);
 			endwin();
@@ -301,8 +307,7 @@ int main()
 			printw("\nIncorrect charecter try again\n\n");
    			printw("\n\nPress any key to continue...");
    			getch();
-
 		}
-	}
+ 	}
 	endwin();
 }
